77ba46aa7a6441ddad62d9612e3ff96eacc3e005
P1c4C0d3 <pablo.fraga@gmail.com>
2019-02-03 14:14:08 -0300

    Server module created

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var http = require("http");
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Context-Type": "text/html"});
-    response.write("Hi!");
-    response.end();
-}).listen(7777);
+function serverStart() {
+    http.createServer(function(request, response) {
+        response.writeHead(200, {"Context-Type": "text/html"});
+        response.write("Hi!");
+        response.end();
+    }).listen(7777);
+    console.log("Server started...");
+}
+exports.start = serverStart;
+
